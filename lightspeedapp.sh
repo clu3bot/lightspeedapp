de632d47e17325b3ed46041bbe2ddf1981a6e74f
Brennan Mccown <clu3bot@protonmail.com>
2024-01-18 14:35:06 -0500

    Update lightspeedapp.sh

--- lightspeedapp.sh
+++ lightspeedapp.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
+#lightspeed quick lookup and auditing tool
+#written by brennan mccown
+file="token.config"
 
-token="lsxs_pt_h9OPozjbJpgm2WiNFnzMY1Oh8V034tjM"
+if [ -e "$file" ]; then
+    token=$(cat token.config)
+else
+   read -p "Enter Access Code" access_code
+   echo $access_code > token.config
+fi
 
 get_giftcard_info(){
     read -p "Enter a GiftCard Number: " giftcard_number
-    curl --request GET \
-     --url https://churchillsgardencenter.vendhq.com/api/2.0/gift_cards/"$giftcard_number" \
-     --header 'accept: application/json' \
-     --header 'authorization: Bearer lsxs_pt_h9OPozjbJpgm2WiNFnzMY1Oh8V034tjM'
+    curl --request GET --url https://churchillsgardencenter.vendhq.com/api/2.0/gift_cards/"$giftcard_number" --header 'accept: application/json' --header 'authorization: Bearer '$token'' > output.config
+    clear
+    current_balance=$(cat output.config | awk -F'[:,]' '{for(i=1;i<=NF;i++){if($i~/"balance"/){print $(i+1)}}}')
+    original_balance=$(cat output.config | awk -F'[:,]' '{for(i=1;i<=NF;i++){if($i~/"total_sold"/){print $(i+1)}}}')
+    since_redeemed=$(cat output.config | awk -F'[:,]' '{for(i=1;i<=NF;i++){if($i~/"total_redeemed"/){print $(i+1)}}}')
+    
+    echo -e "Card Number - $giftcard_number\n"
+    echo -e "Current Balance - $current_balance\n"
+    echo -e "Original Balance - $original_balance\n"
+    echo -e "Redeemed Since Bought - $since_redeemed\n"
+    read -p "Press Enter to Continue to main Menu" 
+    menu
 }
 
 
 menu() {
+    clear
     echo "[1] Get GiftCard Info"
     read -p "Select an Option: " choice
     
